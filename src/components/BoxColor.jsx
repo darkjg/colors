3d248e7ac7dc49086a3f38058012c7f91dded4c5
Jorge <newdarkjg@gmail.com>
2024-04-14 19:08:40 +0200

    Ejerecicio acabado

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -1,7 +1,24 @@
-function boxColor ({color, value}) {
+import { useEffect, useState, useRef } from 'react';
+function boxColor({ color, value }) {
+  const valueRef = useRef(null);
+  const [classList, setClassList] = useState('');
+
+  const boxCSS={
+    color:color,
+    backgroundColor: color === value ? `${color}` : 'none'
+  }
+
+  useEffect(() => {
+    setClassList(valueRef.current.classList)
+    console.log(classList)
+  }, [])
+
   return (
     <>
-    </> 
+      <div ref={valueRef} className={`box ${color}`} style={boxCSS}>
+        {color == value ? <p style={{ color: 'black' }}>Yes, I´m {value} color</p> : <p>I´m not a {value} color</p>}
+      </div>
+    </>
   )
 }
 export default boxColor;  
--- src/components/MyFormChallenge.jsx
+++ src/components/MyFormChallenge.jsx
@@ -1,9 +1,30 @@
+import { useState, useRef } from 'react';
+const colors = ['red', 'green', 'pink', 'yellow', 'purple', 'white', 'blue', 'aqua', 'olive'];
+import BoxColor from './BoxColor';
 
 
 function MyForm() {
-
+  const [color, setColor] = useState("")
+  const inputRef = useRef(null);
   return (
-    <></>
+    <>
+      <input
+        type="text"
+        placeholder="Elige el color"
+        className="inputClass"
+        onChange={() => setColor(inputRef.current.value)}
+        value={color}
+        ref={inputRef}
+      ></input>
+      <ul className='container'>
+        {colors.map((col, index) => {
+     
+          return (<>
+            <li><BoxColor key={index} color={col} value={color} /></li>
+          </>)
+        })}
+      </ul>
+    </>
   );
 }
 
